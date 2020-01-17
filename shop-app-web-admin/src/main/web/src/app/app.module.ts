226a07537b1c9c923e469b492fb80d14ff1d8388
viktor.kulygin <viktor.kulygin@kuehne-nagel.com>
2020-01-17 10:38:15 +0200

    prepare structure for admin app

--- shop-app-web-admin/src/main/web/src/app/app.module.ts
+++ shop-app-web-admin/src/main/web/src/app/app.module.ts
@@ -10,9 +10,15 @@ import { routing } from './app.routing';
 import { MainUiModule } from './modules/main-ui/main-ui.module';
 
 /*Services*/
+import { ProductService } from "./services/product.service";
+import { BasketService } from "./services/basket.service";
 
 /*Components*/
 import { HomeComponent } from './components/home/home.component';
+import {OrdersComponent} from "./components/orders/orders.component";
+import {OrderDetailsComponent} from "./components/odred-details/order-details.component";
+import {ProductComponent} from "./components/product/product.component";
+import {HistoryComponent} from "./components/history/history.component";
 
 /*Popups*/
 
@@ -27,12 +33,22 @@ import { HomeComponent } from './components/home/home.component';
     ],
     declarations: [
         AppComponent,
-        HomeComponent
+        HomeComponent,
+        ProductComponent,
+        OrdersComponent,
+        OrderDetailsComponent,
+        HistoryComponent
     ],
     entryComponents: [
     ],
     providers: [
-        HomeComponent
+        HomeComponent,
+        ProductComponent,
+        OrdersComponent,
+        OrderDetailsComponent,
+        HistoryComponent,
+        ProductService,
+        BasketService
     ],
     bootstrap: [AppComponent]
 })
--- shop-app-web-admin/src/main/web/src/app/app.routing.ts
+++ shop-app-web-admin/src/main/web/src/app/app.routing.ts
@@ -1,8 +1,16 @@
-﻿import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
+﻿import {Routes, RouterModule} from '@angular/router';
+import {HomeComponent} from './components/home/home.component';
+import {OrdersComponent} from "./components/orders/orders.component";
+import {OrderDetailsComponent} from "./components/odred-details/order-details.component";
+import {ProductComponent} from "./components/product/product.component";
+import {HistoryComponent} from "./components/history/history.component";
 
 const appRoutes: Routes = [
     { path: 'home', component: HomeComponent },
+    { path: 'orders', component: OrdersComponent },
+    { path: 'order/:id', component: OrderDetailsComponent },
+    { path: 'product/:id', component: ProductComponent },
+    { path: 'history', component: HistoryComponent },
     { path: '**', redirectTo: 'home' }
 ];
 
